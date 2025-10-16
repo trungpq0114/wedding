975a761df2ceec3fcac83f80e63bd9a68fa2c585
Nguyen Si Duong <duong.nguyen@holdstation.com>
2025-10-16 10:38:52 +0700

    a

---
+++ src/App2.tsx
@@ -0,0 +1,9 @@
+export function App2() {
+  return (
+    <iframe
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      src='./test2.html'
+      title='HTML Content'
+    />
+  );
+}
